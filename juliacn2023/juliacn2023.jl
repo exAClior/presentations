179641c143c1cee5a110150ac92b7e9cf79e5641
Yusheng Zhao <yushengzhao2020@outlook.com>
2023-12-05 20:15:33 +0800

    up

--- juliacn2023/juliacn2023.jl
+++ juliacn2023/juliacn2023.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.32
+# v0.19.29
 
 using Markdown
 using InteractiveUtils
@@ -45,7 +45,7 @@ html"""
 """
 
 # ╔═╡ 7636ca8e-04a8-443a-a7f6-e7fbd70ebce6
-TableOfContents(title="Outline",aside=false)
+TableOfContents(title="Outline")
 
 # ╔═╡ 3b6c88e7-6d8a-46a8-a3fb-b396c611aa09
 begin
@@ -61,6 +61,9 @@ end;
 
 # ╔═╡ aa055b7a-ba85-448e-8031-12ec3979e953
 md"""# Package Introduction
+- It's OSPP project
+- My mentor: Dr. Chen Zhao....
+- Background, why make this project?
 |Dr. [Chen Zhao](https://github.com/ChenZhao44) |[Xiu-zhe (Roger) Luo](https://github.com/Roger-luo)|
 |--------------|------------------|
 |$(RobustLocalResource(urlcz, pathcz,:width=>200, :alt=>"Dr. Chen Zhao", cache=false))|$(RobustLocalResource(urlrl, pathrl, :width=>200, :alt=>"Xiuzhe (Roger) Luo", cache=false))|
@@ -76,6 +79,8 @@ end;
 md"""# Introduction to ZXCalculus
 ## ZX-calculus
 - Graphical Language for Tensor Network
+- explain name, what zx means, special tensors, 
+- What defines calculus : it is the stufy of change. Defines algebra on tensor network. make an example based on algebra $x^2+2xy + y^2 = (x+y)^2$
 - Components of the language:
    1) ZX-Diagram
    2) Rewrite rules
@@ -90,11 +95,9 @@ end;
 
 # ╔═╡ 9cd983bf-063c-414e-8bd9-5cb66a3524a1
 md"""## ZX-Diagrams
-- Graphical Representations of Tensor Networks
-- Tensors Parameterized by:
-  1) Number of Legs
-  2) Angles of Rotation (where applicable)
-- Spiders: $(RobustLocalResource(urlintro, pathspds,:width=>800, :align=>:center, :alt=>"Spiders", cache=false))
+- Z "Spider" and X "Spider" (separate): $(RobustLocalResource(urlintro, pathspds,:width=>800, :align=>:center, :alt=>"Spiders", cache=false))
+- Only connectivity matters, but circuit convention, 
+- Graphical Representations of a specialized Tensor Networks
 """
 
 # ╔═╡ d8c73fec-928f-4174-a737-be2e1de0589e
@@ -106,8 +109,12 @@ end;
 md"""## From Quantum Circuit to ZX-Diagram
 - Direct translation from Quantum Gates into Spiders
 - Examples: $(RobustLocalResource(urlintro, pathtable,:width=>500, :align=>:center, :alt=>"Translation table", cache=false))
-- Universality
+- Universality (give other's reference on linear map universality. Some of ZX Calculus is not . QMA complete)
 """
+
+# ╔═╡ f7eddc42-beb9-4fdd-8b68-c81c55907e41
+#use ZXCalculus.jl to show cnot gate matrix rep is the same as zx 
+#also give H and point out it's a syntax sugar
 
 # ╔═╡ 11f63c7e-f648-4f1d-b09d-3ec15a613174
 md"""## Example of Translation
@@ -121,10 +128,12 @@ begin
 end;
 
 # ╔═╡ 3213c733-672a-4eca-9381-1a690d666eed
-md"""## Rewrite Rules
+md"""## Rewrite Rules(Calculus)
+- **Rules**: $(RobustLocalResource(urlintro, pathzxrule,:width=>800, :align=>:center, :alt=>"Translation table", cache=false))
+- What's the use. connect to previous algebra example
 - **Completeness**: "A graphical calculus is complete when its rewrite rules are powerful enough to prove any true equation."[^ZXWorking]
 - **Practical Implication**: Equivalence of Linear Maps through Transformative Sequences in ZX Diagrams
-- **Rules**: $(RobustLocalResource(urlintro, pathzxrule,:width=>800, :align=>:center, :alt=>"Translation table", cache=false))
+
 """
 
 # ╔═╡ 5a01eef2-8893-4ecc-ac0b-7c1320852068
@@ -133,7 +142,7 @@ begin
 end;
 
 # ╔═╡ 6bbf81a9-1fa8-4b83-8663-307ed886446a
-md"""## Soundness
+md"""## Soundness (Useless, remove)
 - **Definition of Soundness**: "A rewrite rule is sound when it preserves the underlying semantics."[^ZXWorking]
 - **Semantics in ZX-Calculus**: Representation of Linear Maps Demonstrating Soundness through ZXCalculus.jl:
 - **Example**: Fusion Rule $(RobustLocalResource(urlintro, pathfusion,:width=>300, :align=>:center, :alt=>"Translation table", cache=false))
@@ -159,10 +168,13 @@ begin
 end
 
 # ╔═╡ 548182df-6533-4d84-8373-8e0678c3cf82
-vizcircuit(zxwd2;graphwidth=10,graphheight=5,density=0.1)
+vizcircuit(zxwd2;graphwidth=10,graphheight=5,density=0.1)#change color
 
 # ╔═╡ ca477e83-5bf0-42ef-8f59-6446241b97ef
 @assert to_matrix(zxwd) ≈ to_matrix(zxwd2)
+
+# ╔═╡ 04347137-2e50-4c9f-b270-9a237b0ca0d3
+# what I did, ZXW Calculus, W calculus definition + reference
 
 # ╔═╡ 96ac3039-6a4d-4f06-94a2-f0369ff414c3
 begin
@@ -186,6 +198,9 @@ begin
 	push_gate!(mizxwd,Val(:X),2,1)
 end;
 
+# ╔═╡ c399194e-cd90-44ca-90ac-7c12e515d72b
+mizxwd.mg
+
 # ╔═╡ 2340c1ba-d90b-4f57-9446-5d4ed8513ed6
 vizcircuit(mizxwd; verbose=true)
 
@@ -194,14 +209,25 @@ begin
 	mizxwd2 = copy(mizxwd)
 	mimatched_pos = match(Rule(:s1),mizxwd2)
 	println(mimatched_pos)
-	mizxwd2 = rewrite!(Rule(:s1),mizxwd2,[mimatched_pos[1]])
+	mizxwd2 = rewrite!(Rule(:s1),mizxwd2,mimatched_pos)
 end
 
+# ╔═╡ ba42651d-c52b-4b5a-b9bc-88d412f24fbe
+begin
+	mizxwd3 = copy(mizxwd)
+	mimatched_pos3 = match(Rule(:s1),mizxwd3)
+	println(mimatched_pos3)
+	mizxwd3 = rewrite!(Rule(:s1),mizxwd3,[mimatched_pos3[1]])
+end
+
+# ╔═╡ f878297f-3ce9-4011-a6d4-cd431e24b9a7
+length(mizxwd3.st)
+
 # ╔═╡ 560545c2-51da-434a-9324-3540d9857c79
-mizxwd2
+length(mizxwd2.st)
 
 # ╔═╡ 1350756d-6a2b-4a15-b0e0-db8ec1baa333
-vizcircuit(mizxwd2)
+vizcircuit(mizxwd3;verbose=true)
 
 # ╔═╡ ab4f76a8-10c5-4dee-8b99-446bb095a65c
 md"""### Circuit Simplification
@@ -225,7 +251,8 @@ md"""## Application
 """
 
 # ╔═╡ d516b5ed-69f9-41d7-8627-7df14a740e7c
-md"""# Thank you!
+md"""# Summary
+Thank you!
 """
 
 # ╔═╡ 10af6d43-453e-46e0-b983-eb00c53e8a68
@@ -242,27 +269,32 @@ md"""
 # ╟─bd7c63de-cb71-41d2-ae99-8b8807513f25
 # ╟─aa055b7a-ba85-448e-8031-12ec3979e953
 # ╟─166384c5-b083-4792-a397-2f9495a3bc7b
-# ╟─87538743-fed0-4db1-bc6b-a2f76b68cd5a
+# ╠═87538743-fed0-4db1-bc6b-a2f76b68cd5a
 # ╟─706f074a-95ea-4d3a-8ef6-b355f1723a01
-# ╟─9cd983bf-063c-414e-8bd9-5cb66a3524a1
+# ╠═9cd983bf-063c-414e-8bd9-5cb66a3524a1
 # ╟─d8c73fec-928f-4174-a737-be2e1de0589e
-# ╟─6330319d-9425-4619-95cc-7f5bd7eca9bf
+# ╠═6330319d-9425-4619-95cc-7f5bd7eca9bf
+# ╠═f7eddc42-beb9-4fdd-8b68-c81c55907e41
 # ╟─11f63c7e-f648-4f1d-b09d-3ec15a613174
 # ╟─1607afe4-9e58-4211-b471-83a4c6f504aa
-# ╟─3213c733-672a-4eca-9381-1a690d666eed
+# ╠═3213c733-672a-4eca-9381-1a690d666eed
 # ╟─5a01eef2-8893-4ecc-ac0b-7c1320852068
-# ╟─6bbf81a9-1fa8-4b83-8663-307ed886446a
+# ╠═6bbf81a9-1fa8-4b83-8663-307ed886446a
 # ╠═3967f9c9-5f72-4406-83f0-7cd0cd9648ce
 # ╠═51b2c6b7-78f4-401e-899b-dab3bd8a4208
 # ╠═0d78bec4-e737-4eef-aa9a-b0d2bb50c309
 # ╠═bfdf1671-d300-44db-8d2b-9e5fe5cdbed3
 # ╠═548182df-6533-4d84-8373-8e0678c3cf82
 # ╠═ca477e83-5bf0-42ef-8f59-6446241b97ef
+# ╠═04347137-2e50-4c9f-b270-9a237b0ca0d3
 # ╟─96ac3039-6a4d-4f06-94a2-f0369ff414c3
 # ╟─c7fe41a9-4b2d-4de4-8657-f6d1a831cfc2
 # ╠═fb8fc69c-a77f-4442-8ed3-ff1d5ae3911c
+# ╠═c399194e-cd90-44ca-90ac-7c12e515d72b
 # ╠═2340c1ba-d90b-4f57-9446-5d4ed8513ed6
 # ╠═219bd61d-da08-4b04-b271-bc6a52c3c29b
+# ╠═ba42651d-c52b-4b5a-b9bc-88d412f24fbe
+# ╠═f878297f-3ce9-4011-a6d4-cd431e24b9a7
 # ╠═560545c2-51da-434a-9324-3540d9857c79
 # ╠═1350756d-6a2b-4a15-b0e0-db8ec1baa333
 # ╟─ab4f76a8-10c5-4dee-8b99-446bb095a65c
