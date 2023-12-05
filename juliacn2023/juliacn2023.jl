6baab0089a43bfdfad1b208cc88460fc5d3af64c
Yusheng Zhao <yushengzhao2020@outlook.com>
2023-12-05 11:16:02 +0800

    update tutorial

--- juliacn2023/juliacn2023.jl
+++ juliacn2023/juliacn2023.jl
@@ -16,7 +16,8 @@ end;
 # ╔═╡ 3967f9c9-5f72-4406-83f0-7cd0cd9648ce
 begin
 	using ZXCalculus.ZXW
-	using ZXCalculus.ZXW:Z, add_spider!, Parameter, insert_spider!, add_edge!
+	using ZXCalculus.ZXW:Z, add_spider!, Parameter, insert_spider!, add_edge!, rewrite!, match
+	using ZXCalculus.Application: to_matrix
 end;
 
 # ╔═╡ 0f6ad1ea-39b2-4197-8f00-5afa55839be2
@@ -150,14 +151,61 @@ end;
 # ╔═╡ 0d78bec4-e737-4eef-aa9a-b0d2bb50c309
 vizcircuit(zxwd;graphwidth=10,graphheight=5,density=0.1)
 
-# ╔═╡ ca477e83-5bf0-42ef-8f59-6446241b97ef
+# ╔═╡ bfdf1671-d300-44db-8d2b-9e5fe5cdbed3
+begin
+	zxwd2 = copy(zxwd)
+	matched_pos = match(Rule(:s1),zxwd2)
+	zxwd2 = rewrite!(Rule(:s1),zxwd2,matched_pos)
+end
 
+# ╔═╡ 548182df-6533-4d84-8373-8e0678c3cf82
+vizcircuit(zxwd2;graphwidth=10,graphheight=5,density=0.1)
+
+# ╔═╡ ca477e83-5bf0-42ef-8f59-6446241b97ef
+@assert to_matrix(zxwd) ≈ to_matrix(zxwd2)
+
+# ╔═╡ 96ac3039-6a4d-4f06-94a2-f0369ff414c3
+begin
+	pathmag = "figs/magicinjection.png"
+	pathmagzx = "figs/magicinjectionzx.png"
+end;
+
+# ╔═╡ c7fe41a9-4b2d-4de4-8657-f6d1a831cfc2
+md"""## Applications
+### Understanding Magic State Injection
+- $(RobustLocalResource(urlintro, pathmag,:width=>300, :align=>:center, :alt=>"Translation table", cache=false)) 
+- $(RobustLocalResource(urlintro, pathmagzx,:width=>800, :align=>:center, :alt=>"Translation table", cache=false))
+"""
+
+# ╔═╡ fb8fc69c-a77f-4442-8ed3-ff1d5ae3911c
+begin
+	mizxwd = ZXWDiagram(2)
+	push_gate!(mizxwd,Val(:Z),2,1//4)
+	push_gate!(mizxwd,Val(:CNOT),2,1)
+	push_gate!(mizxwd,Val(:Z),1,1//2)
+	push_gate!(mizxwd,Val(:X),2,1)
+end;
+
+# ╔═╡ 2340c1ba-d90b-4f57-9446-5d4ed8513ed6
+vizcircuit(mizxwd; verbose=true)
+
+# ╔═╡ 219bd61d-da08-4b04-b271-bc6a52c3c29b
+begin
+	mizxwd2 = copy(mizxwd)
+	mimatched_pos = match(Rule(:s1),mizxwd2)
+	println(mimatched_pos)
+	mizxwd2 = rewrite!(Rule(:s1),mizxwd2,[mimatched_pos[1]])
+end
+
+# ╔═╡ 560545c2-51da-434a-9324-3540d9857c79
+mizxwd2
+
+# ╔═╡ 1350756d-6a2b-4a15-b0e0-db8ec1baa333
+vizcircuit(mizxwd2)
 
 # ╔═╡ ab4f76a8-10c5-4dee-8b99-446bb095a65c
-md"""## Applications
-- Magic State Distillation
-- Phase Teleporations
-
+md"""### Circuit Simplification
+- Via Phase Teleporation
 """
 
 # ╔═╡ 02390a36-06fe-4fb7-b00f-46461542a8aa
@@ -175,30 +223,6 @@ md"""## Application
 - gradient calculation
 
 """
-
-# ╔═╡ dd1580cf-c7f3-4f2b-9141-09585ca7e36c
-
-
-# ╔═╡ 759d5cef-c63c-4deb-9876-3707f21da8cd
-
-
-# ╔═╡ f41bd7ef-c4b6-44b0-a920-90f1dd5019e5
-
-
-# ╔═╡ b0d972d9-1d0d-461b-befa-384a3453778b
-
-
-# ╔═╡ d6eeb36e-f52b-46a1-9cbb-64c1eef5c1e7
-
-
-# ╔═╡ d9e41ac3-c3a4-41e3-abd8-5bdf41d27ab7
-
-
-# ╔═╡ c73a88a9-8251-47e9-a5fd-e6a83b1f528b
-
-
-# ╔═╡ a28cddd3-717e-4cd6-9178-77019818993f
-
 
 # ╔═╡ d516b5ed-69f9-41d7-8627-7df14a740e7c
 md"""# Thank you!
@@ -231,18 +255,19 @@ md"""
 # ╠═3967f9c9-5f72-4406-83f0-7cd0cd9648ce
 # ╠═51b2c6b7-78f4-401e-899b-dab3bd8a4208
 # ╠═0d78bec4-e737-4eef-aa9a-b0d2bb50c309
+# ╠═bfdf1671-d300-44db-8d2b-9e5fe5cdbed3
+# ╠═548182df-6533-4d84-8373-8e0678c3cf82
 # ╠═ca477e83-5bf0-42ef-8f59-6446241b97ef
-# ╠═ab4f76a8-10c5-4dee-8b99-446bb095a65c
+# ╟─96ac3039-6a4d-4f06-94a2-f0369ff414c3
+# ╟─c7fe41a9-4b2d-4de4-8657-f6d1a831cfc2
+# ╠═fb8fc69c-a77f-4442-8ed3-ff1d5ae3911c
+# ╠═2340c1ba-d90b-4f57-9446-5d4ed8513ed6
+# ╠═219bd61d-da08-4b04-b271-bc6a52c3c29b
+# ╠═560545c2-51da-434a-9324-3540d9857c79
+# ╠═1350756d-6a2b-4a15-b0e0-db8ec1baa333
+# ╟─ab4f76a8-10c5-4dee-8b99-446bb095a65c
 # ╠═02390a36-06fe-4fb7-b00f-46461542a8aa
 # ╠═2ba578c3-1ae0-45f7-aec8-445fabf7a911
 # ╠═2b818577-8ba6-4939-b6ff-2572b169ab63
-# ╠═dd1580cf-c7f3-4f2b-9141-09585ca7e36c
-# ╠═759d5cef-c63c-4deb-9876-3707f21da8cd
-# ╠═f41bd7ef-c4b6-44b0-a920-90f1dd5019e5
-# ╠═b0d972d9-1d0d-461b-befa-384a3453778b
-# ╠═d6eeb36e-f52b-46a1-9cbb-64c1eef5c1e7
-# ╠═d9e41ac3-c3a4-41e3-abd8-5bdf41d27ab7
-# ╠═c73a88a9-8251-47e9-a5fd-e6a83b1f528b
-# ╠═a28cddd3-717e-4cd6-9178-77019818993f
 # ╠═d516b5ed-69f9-41d7-8627-7df14a740e7c
 # ╟─10af6d43-453e-46e0-b983-eb00c53e8a68
